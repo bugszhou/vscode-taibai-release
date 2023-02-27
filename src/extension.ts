7e14c48ea8312ea42cb6f102d86367bfc5890b5a
youzhao.zhou <youzhao.zhou@verystar.cn>
2023-02-27 21:58:03 +0800

    feat: 增加发版时间

--- src/extension.ts
+++ src/extension.ts
@@ -131,7 +131,13 @@ function writeReleaseMD(
     listStr = `\<ul\>\n` + recordsHtmlArr.join("\n") + `\</ul\>\n\n`;
   }
 
-  mds.splice(1, 0, `<h2>v${version}</h2>`, ...listStr.split("\n"));
+  mds.splice(
+    1,
+    0,
+    `<h2>v${version}</h2>\n`,
+    `<p><strong>发版时间：${dateFormat(Date.now(), "yyyy-MM-dd hh-mm-ss")}</strong></p>`,
+    ...listStr.split("\n"),
+  );
 
   return mds.join("\n") + "\n";
 }
@@ -142,4 +148,39 @@ function getListStr(content = "", index = 0) {
 <li>${content}</li>
 </ol>
 </li>`;
+}
+
+function dateFormat(millisecond: number, fmt: string): string {
+  let str = fmt;
+  const date = new Date(millisecond);
+  if (!date || date.toString() === "Invalid Date") {
+    return "";
+  }
+  const o: any = {
+    "M+": date.getMonth() + 1, // 月份
+    "d+": date.getDate(), // 日
+    "h+": date.getHours(), // 小时
+    "m+": date.getMinutes(), // 分
+    "s+": date.getSeconds(), // 秒
+    "q+": Math.floor((date.getMonth() + 3) / 3), // 季度
+    S: date.getMilliseconds(), // 毫秒
+  };
+
+  if (/(y+)/.test(str)) {
+    str = str.replace(
+      RegExp.$1,
+      `${date.getFullYear()}`.substr(4 - RegExp.$1.length),
+    );
+  }
+
+  Object.keys(o).forEach((k) => {
+    if (new RegExp(`(${k})`).test(str)) {
+      str = str.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : `00${o[k]}`.substr(`${o[k]}`.length),
+      );
+    }
+  });
+
+  return str;
 }
